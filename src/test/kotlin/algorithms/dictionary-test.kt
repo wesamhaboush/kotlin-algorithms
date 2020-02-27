d45bb9e18c8cc9b561a46b02f139fa9a04321ff3
Wesam Haboush <wesam.haboush@gmail.com>
2020-02-26 22:02:11 -0800

    basics of the dictionary

--- src/test/kotlin/algorithms/dictionary-test.kt
+++ src/test/kotlin/algorithms/dictionary-test.kt
@@ -6,7 +6,7 @@ class `dictionary-test` {
 
     @Test
     fun test1() {
-        val chars = listOf('y', 'u', 'o', 'w', 'd', 's')
+        val chars = listOf('d', 'i', 'z', 'r', 'o', 's')
         val noChars = listOf(
                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
                 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
@@ -16,10 +16,12 @@ class `dictionary-test` {
         time {
             readDictionary2()
                     .map { it.toLowerCase() }
-                    .filter { it.length in 3..8 }
-                    .filter { frequenyMap(it.filter { it != 'd' }).all { it.value < 2 } }
-                    .filter { it.toList().count { chars.contains(it) } > 3 }
-                    .filter { it.toList().count { noChars.contains(it) } == 0 }
+                    .filter { it.length in 3..8 } // minimum and maximum number of letters
+                    .filter { frequenyMap(it.filter { it != 'r' }).all { it.value < 2 } } // frequency of letter with exceptions
+                    .filter { it.toList().count { chars.contains(it) } > 3 } // how many correct letters
+//                    .filter { it.toList().count { noChars.contains(it) } == 0 } // how many different chars
+                    .filter { it.toList().containsAll(listOf('m', 'o')) } // must letters
+                    .filter { !it.toList().any { it in listOf('v', 't', 'a', 'p', 'n', 'e') } } // forbidden letters
                     .onEach { println(it) }
                     .toList()
         }
